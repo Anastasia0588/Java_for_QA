9dcfce12e0422c479e8416a5f6cc1c6fc0dc0dd9
Anastasia <a.drozdovskaia@yandex.ru>
2021-03-03 09:13:30 +0300

    Удалено лишнее при решении задачи 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,19 +1,18 @@
-/*package ru.stqa.pft.sandbox;
+package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 public static void main (String[] args){
-    hello("world");
 
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+    Point p1 = new Point(2.0, 4);
+    Point p2 = new Point(3, 5);
+    System.out.println("Расстояние между тчками p1 (" + p1.x + ";" + p1.y + ") и p2 = " + distance(p1, p2));
+}
 
-    Rectangle r = new Rectangle(4, 3);
-    System.out.println("Площадь прямоугольника со сторонами а = " + r.a + " и b = " + r.b + " = " + r.area());
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    }
 
 }
 
-public static void hello(String somebody){
-       System.out.println("Hello, " + somebody + "!");
-}
 
-*/
+
