6c482eadb73047ab053778273ec1621be15e7b3d
Anastasia <a.drozdovskaia@yandex.ru>
2021-03-09 22:31:10 +0300

    Созданы вспомогательные методы и объекты

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -28,7 +28,7 @@ public class GroupCreationTests {
   @Test
   public void testGroupCreation() throws Exception {
     initGroupCreation();
-    fillGruopForm("Test1", "TEST2", "TEST3");
+    fillGruopForm(new GroupData("Test1", "TEST2", "TEST3"));
     submitGroupCreation();
     returnToGroupPage();
 
@@ -42,16 +42,16 @@ public class GroupCreationTests {
     wd.findElement(By.name("submit")).click();
   }
 
-  private void fillGruopForm(String name, String header, String footer) {
+  private void fillGruopForm(GroupData groupData) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(name);
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(header);
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(footer);
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
   }
 
   private void initGroupCreation() {
