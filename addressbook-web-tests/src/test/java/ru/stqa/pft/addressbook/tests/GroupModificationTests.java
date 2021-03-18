64d805b8884dd0757fcfa1739f3282d884919d69
Anastasia <a.drozdovskaia@yandex.ru>
2021-03-18 22:07:41 +0300

    Тест для модификации групп GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void TestGroupModification() {
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModifacation();
+        app.getGroupHelper().fillGroupForm(new GroupData("RenameGroup", "Test11", "Test22"));
+        app.getGroupHelper().submitModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
