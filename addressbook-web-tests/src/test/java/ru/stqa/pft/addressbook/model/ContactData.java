24551513f27463b253969602e605daff4a4423b6
Anastasia <a.drozdovskaia@yandex.ru>
2021-03-18 20:59:11 +0300

    Пересохранение теста для создания контакта
    Тест успешный

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook.model;
+
+public class ContactData {
+    private final String name;
+    private final String lastName;
+    private final String company;
+    private final String city;
+    private final String phoneNumber;
+    private final String email;
+
+    public ContactData(String name, String lastName, String company, String city, String phoneNumber, String email) {
+        this.name = name;
+        this.lastName = lastName;
+        this.company = company;
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
