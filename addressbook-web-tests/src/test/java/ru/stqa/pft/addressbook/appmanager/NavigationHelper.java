6fdeae272f7fb3c44a06a821ee3868b3eaf6c6ad
Anastasia <a.drozdovskaia@yandex.ru>
2021-03-28 21:25:33 +0300

    "Ленивые" действия 3.9
    Модификации в NavigationHelper. Проверки находждения на нужных страницах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -10,10 +10,18 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void goToGroupPage() {
+        if (isElementPresentMy(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresentMy(By.name("new"))){
+        return;
+        }
        click(By.linkText("groups"));
     }
 
     public void goToHomePage() {
+        if (isElementPresentMy(By.id("maintable"))){
+            return;
+        }
       click(By.linkText("home"));
     }
 
