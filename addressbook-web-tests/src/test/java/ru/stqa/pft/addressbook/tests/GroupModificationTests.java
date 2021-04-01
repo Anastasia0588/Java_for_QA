1ac4e9f2df02b06576d74b1700bd96ca6bd5613b
Anastasia <a.drozdovskaia@yandex.ru>
2021-04-01 22:20:32 +0300

    Формирование коллекции 4.5
    Сравниваем размер получаемых списков групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
 
     @Test
@@ -12,14 +14,14 @@ public class GroupModificationTests extends TestBase {
                if (! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("Test1", null, "TEST3"));
         }
-        int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().selectGroup(before - 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModifacation();
         app.getGroupHelper().fillGroupForm(new GroupData("RenameGroup1", "Test11", "Test22"));
         app.getGroupHelper().submitModification();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(before, after);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(before.size(), after.size());
         app.getNavigationHelper().goToHomePage();
     }
 }
