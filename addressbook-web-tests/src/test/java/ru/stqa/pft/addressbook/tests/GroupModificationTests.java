574085688f75dabd7cfc31333d88a3d38cf73a46
Anastasia <a.drozdovskaia@yandex.ru>
2021-04-01 21:49:43 +0300

    Проверка количества элементов на странице Л4.3

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,14 +9,17 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void TestGroupModification() {
         app.getNavigationHelper().goToGroupPage();
-        if (! app.getGroupHelper().isThereAGroup()){
+               if (! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("Test1", null, "TEST3"));
         }
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModifacation();
         app.getGroupHelper().fillGroupForm(new GroupData("RenameGroup", "Test11", "Test22"));
         app.getGroupHelper().submitModification();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(before, after);
         app.getNavigationHelper().goToHomePage();
     }
 }
