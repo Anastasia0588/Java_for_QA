a4e6c7469f06ceb680e2efed7ebdc74e249e4e5d
Anastasia <a.drozdovskaia@yandex.ru>
2021-04-02 20:34:41 +0300

    Сравнение списков групп перед и после удаления Л.4.6
    Сравнение коллекций

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -21,6 +21,9 @@ public class GroupDeletionTests extends TestBase{
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
     app.getNavigationHelper().goToHomePage();
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
 
 }
