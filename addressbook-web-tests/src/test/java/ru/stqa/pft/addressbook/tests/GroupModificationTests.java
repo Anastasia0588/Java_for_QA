75a10c8ce2013be56ca28e486a174a8334efda9d
Anastasia <a.drozdovskaia@yandex.ru>
2021-04-03 12:15:18 +0300

    Добавлена проверка получившихся списков Л4.7
    Сравнение элементов без учета порядка. Перед сравнением списки преобразовываются в множества.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -17,11 +18,16 @@ public class GroupModificationTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModifacation();
-        app.getGroupHelper().fillGroupForm(new GroupData("RenameGroup1", "Test11", "Test22"));
+        GroupData groupData = new GroupData(before.get(before.size() - 1).getId(), "RenameGroup3", "Test11", "Test22");
+        app.getGroupHelper().fillGroupForm(groupData);
         app.getGroupHelper().submitModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(before.size(), after.size());
         app.getNavigationHelper().goToHomePage();
+
+        before.remove(before.size() - 1);
+        before.add(groupData);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
