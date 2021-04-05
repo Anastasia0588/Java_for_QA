451ab094a6a1f24d78bd6f6b30ee6ab24a7cb908
Anastasia <a.drozdovskaia@yandex.ru>
2021-04-05 14:05:53 +0300

    Задание 9.  Сравнение списков в тестах для создания, удаления и модификации групп и контактов
    Исправлено в тесте Создание контакта.
    Оптимизированы импорты

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,13 +1,10 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.checkerframework.checker.units.qual.C;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase{
@@ -42,7 +39,8 @@ public class ContactCreationTests extends TestBase{
     */
 
     //сравнение остортированных списков:
-    after.remove(after.size() - 1);
+
+    before.add(contact);
     Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
     before.sort(byId);
     after.sort(byId);
