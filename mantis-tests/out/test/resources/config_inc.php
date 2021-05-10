572b5296433c33644b3e3bbb479f1ab79fca355d
Anastasia <a.drozdovskaia@yandex.ru>
2021-05-10 10:00:44 +0300

    Получение почты на встроенный в тесты почтовый сервер Л.8.6

--- mantis-tests/out/test/resources/config_inc.php
+++ mantis-tests/out/test/resources/config_inc.php
@@ -4,7 +4,9 @@ $g_db_type                = 'mysqli';
 $g_database_name          = 'bugtracker';
 $g_db_username            = 'root';
 $g_db_password            = '';
-$g_signup_use_captcha     = 'OFF';
+$g_signup_use_captcha     = 'OFF'; // отключение защиты отроботов
+$g_phpMailer_method       = PHPMAILER_METHOD_SMTP;  // способ доставки почты по протоколу SMTP
+$g_smtp_host              = 'localhost';  // адрес доставки почты - т.е. текущая машина
 
 $g_default_timezone       = 'Europe/Berlin';
 
